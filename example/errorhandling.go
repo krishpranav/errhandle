c0f12d6331cbcc9fd88873bae8a177088651345f
krishpranav <krisna.pranav@gmail.com>
2022-01-03 17:31:50 +0530

    example: errorhandling

--- example/errorhandling.go
+++ example/errorhandling.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/krishpranav/errhandle"
+)
+
+func main() {
+	myerrHandler := errhandle.NewErrorHandler()
+
+	var SomeData string
+
+	myerrHandler.AddHandler(func(err error, data ...interface{}) interface{} {
+		fmt.Println("Generic Error Occured")
+		return SomeData
+	})
+
+}
