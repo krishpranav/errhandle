7fc84b3dd76be26e97860a425a4c37880b53b6e1
krishpranav <krisna.pranav@gmail.com>
2022-01-03 17:19:33 +0530

    errhandle

--- errhandle.go
+++ errhandle.go
@@ -1,16 +1,18 @@
-package errhandle
+package falcon
 
-import "reflect"
-
-type ErrorHandler struct {
-	handlers       map[reflect.Type]func(error, ...interface{}) interface{}
-	defaultHandler func(error, ...interface{})
-}
+import (
+	"reflect"
+)
 
 func NewErrorHandler() *ErrorHandler {
 	handler := ErrorHandler{}
 	handler.Init()
 	return &handler
+}
+
+type ErrorHandler struct {
+	handlers       map[reflect.Type]func(error, ...interface{}) interface{}
+	defaultHandler func(error, ...interface{}) interface{}
 }
 
 func (errorHandler *ErrorHandler) Init() {
@@ -26,4 +28,16 @@ func (errorHandler *ErrorHandler) AddHandler(handleFunc func(error, ...interface
 	}
 	errType := reflect.TypeOf(errTypes[0])
 	errorHandler.handlers[errType] = handleFunc
+}
+
+func (errorHandler ErrorHandler) Check(err error, data ...interface{}) interface{} {
+	if err == nil {
+		return nil
+	}
+	errType := reflect.TypeOf(err)
+	handler, valid := errorHandler.handlers[errType]
+	if !valid {
+		handler = errorHandler.defaultHandler
+	}
+	return handler(err, data)
 }
