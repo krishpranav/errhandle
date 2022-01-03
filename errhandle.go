f5c4dbdf19ab3d3074c8fa3d68a942d84cecf667
krishpranav <krisna.pranav@gmail.com>
2022-01-03 17:19:16 +0530

    errhandle

--- errhandle.go
+++ errhandle.go
@@ -16,3 +16,14 @@ func NewErrorHandler() *ErrorHandler {
 func (errorHandler *ErrorHandler) Init() {
 	errorHandler.handlers = map[reflect.Type](func(error, ...interface{}) interface{}){}
 }
+
+func (errorHandler *ErrorHandler) AddHandler(handleFunc func(error, ...interface{}) interface{},
+	errTypes ...interface{}) {
+
+	if errTypes == nil {
+		errorHandler.defaultHandler = handleFunc
+		return
+	}
+	errType := reflect.TypeOf(errTypes[0])
+	errorHandler.handlers[errType] = handleFunc
+}
