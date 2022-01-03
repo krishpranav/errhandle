6349bc7635088ea783677866be4be7e0ed456cca
krishpranav <krisna.pranav@gmail.com>
2022-01-03 17:24:42 +0530

    errhandling

--- errhandle_test.go
+++ errhandle_test.go
@@ -1,5 +1,12 @@
 package errhandle
 
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
 var defaultErrOccured bool
 var customErrOccured bool
 var dataPassed bool
@@ -8,4 +15,33 @@ type customErr struct{}
 
 func (ce customErr) Error() string {
 	return "custom error"
+}
+
+func TestErrHandler(t *testing.T) {
+	errHandler := NewErrorHandler()
+	assert := assert.New(t)
+
+	assert.NotNil(errHandler)
+	assert.IsType(&ErrorHandler{}, errHandler)
+	assert.NotNil(errHandler.handlers)
+	assert.Nil(errHandler.defaultHandler)
+
+	errHandler.AddHandler(func(err error, data ...interface{}) interface{} {
+		defaultErrOccured = true
+		dataPassed = data != nil
+		return true
+	})
+	errHandler.AddHandler(func(err error, data ...interface{}) interface{} {
+		customErrOccured = true
+		return true
+	}, customErr{})
+
+	assert.True(errHandler.Check(errors.New("test"), "test").(bool))
+	assert.True(errHandler.Check(customErr{}).(bool))
+
+	assert.Nil(errHandler.Check(nil))
+
+	assert.True(defaultErrOccured)
+	assert.True(customErrOccured)
+	assert.True(dataPassed)
 }
